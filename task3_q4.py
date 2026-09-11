af54b29bc9e084b736d6db76634e76b99bc24849
Hlumelo-gif <hlumelomasinyane3@gmail.com>
2026-09-11 18:58:27 +0200

    Implement sum of user-input numbers
    Added a loop to sum user-input numbers until 0 is entered.

--- task3_q4.py
+++ task3_q4.py
@@ -1,1 +1,13 @@
+# Question 3
+total = 0
 
+# Get the first number
+num = int(input("Enter a number (0 to stop): "))
+
+# Keep looping until they enter 0
+while num != 0:
+total = total + num
+# Ask for the next number
+num = int(input("Enter a number (0 to stop): "))
+
+print("The total sum is:", total)
