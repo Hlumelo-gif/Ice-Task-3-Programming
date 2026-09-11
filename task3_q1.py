002918532e97af2c78a0397429968840e587dc09
Hlumelo-gif <hlumelomasinyane3@gmail.com>
2026-09-11 18:30:04 +0200

    Add number guessing game in task3_q1.py
    Implement number guessing game logic.

--- task3_q1.py
+++ task3_q1.py
@@ -1,1 +1,14 @@
+# Question 1
+secret = 65
+guess = 0
 
+# Keep looping until they guess the correct number
+while guess != secret:
+guess = int(input("Guess the number: "))
+
+if guess < secret:
+print("Too low!")
+elif guess > secret:
+print("Too high!")
+
+print("Correct! You guessed the number.")
